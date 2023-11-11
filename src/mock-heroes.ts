3cfd8060889ad7b8ee60477824badfcf4341dfc4
David Tentser <s6datent@pares.de>
2023-11-11 12:08:42 +0100

    Staging edited files

---
+++ src/mock-heroes.ts
@@ -0,0 +1,6 @@
+import {Hero} from "./app/hero";
+
+export const HEROES: Hero[] = [ {id: 1, name:'Batman'}, {id: 2, name: 'Superman'},{id:3 , name: 'Green Lantern'},
+  {id:4 , name:'Black Ant'}]
+
+
