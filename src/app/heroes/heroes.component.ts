7d3e62a99daa5953fe1f896ee62e3ec711846579
David Tentser <s6datent@pares.de>
2023-11-12 11:49:58 +0100

    Hero list clearing in

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {Hero} from "../hero";
 import {HEROES} from "../../mock-heroes";
+import {HeroService} from "../hero.service";
+import {MessageService} from "../message.service";
+
 
 @Component({
   selector: 'app-heroes',
@@ -8,11 +11,24 @@ import {HEROES} from "../../mock-heroes";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero | undefined;
+
+  ngOnInit(){
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero){
     this.selectedHero = hero;
+    this.messageService.add(`Hero ${this.selectedHero?.name} with id ${this.selectedHero?.id} chosen`);
+  }
+
+  getHeroes(){
+   this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+   this.messageService.add(`Hero ${this.selectedHero?.name} with id ${this.selectedHero?.id} chosen`);
+  }
+
+  constructor(private heroService: HeroService, private messageService: MessageService) {
   }
 
 
